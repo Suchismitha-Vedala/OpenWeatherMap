3d2a9f3b859662a3bb7601e41102e674739b2ae8
Suchismitha <svedala@github.com>
2018-07-10 16:40:55 -0500

    added temperature validation

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 api_key = "b9428de8e646a42987eee11eced64b1b"
-url_5day= 'http://api.openweathermap.org/data/2.5/forecast/city?id='
+url_5day= "http://api.openweathermap.org/data/2.5/forecast?id="
 url_16day ="http://api.openweathermap.org/data/2.5/forecast/daily?id="
 url_weather = "http://api.openweathermap.org/data/2.5/weather?APPID="
 url_view = "https://openweathermap.org/city/"
-city_id=[5977783,4699066,2646507]
+city_id=[6255152,1262963,4068296]
--- program.py
+++ program.py
@@ -8,29 +8,54 @@ import webbrowser
 def Insert_Database(data, report_type):
 	client = MongoClient('mongodb://localhost:27017')
 	db = client['Open_Weather_API']
-	if(report_type=="5day/3hr"):
+	if(report_type=="fiveday"):
 		test=db.fiveday
 	if(report_type=="16day"):
 		test=db.sixteenday
 	if(report_type=="weather_map"):
 		test=db.weather_map
 	test.insert(data)
-	print("Inserted into Database", report_type)
+	#print("Inserted into Database", report_type)
 
 def get_data(url,report_type):
-	time.sleep(3)#making sure only 1 API call is made per second.
+	time.sleep(30)#making sure only 1 API call is made per second.
 	response = urlopen(url).read().decode('utf-8')
 	obj = json.loads(response)
 	Insert_Database(obj, report_type)
+	if(report_type=="weather_map"):
+		if('main' in obj.keys()):
+			if (obj['main']['temp_min']<256.484):
+				print "AlERT! Temperature less than 2 degree Farenheit in ", str(obj['name']) 
+	else:
+		for each in range(len(obj)):
+			if(obj['list'][each]['main']['temp_min']<256.484):
+				print "AlERT! Temperature less than 2 degree Farenheit in ", str(obj['city']['name']), " on ", str(obj['list'][each]['dt_txt'])
+			if (('snow' in obj['list'][each].keys()) and any(obj['list'][each]['snow'])):
+				print "It is snowing in ", str(obj['city']['name']), " on ", str(obj['list'][each]['dt_txt'])
+			if (('rain' in obj['list'][each].keys()) and any(obj['list'][each]['rain'])):
+				print "It is Raining in ", str(obj['city']['name']), " on ", str(obj['list'][each]['dt_txt'])
+				
+			'''
+			if(obj['list'][each]['main']['temp_min']<256.484):
+				print("AlERT! Temperature less than 2 degree Farenheit in ", obj['city']['name'], " on ", obj['list'][each]['dt_txt'])
+			if (('snow' in obj['list'][each].keys()) and any(obj['list'][each]['snow'])):
+				print("It is snowing in ", obj['city']['name'], " on ", obj['list'][each]['dt_txt'])
+			if (('rain' in obj['list'][each].keys()) and any(obj['list'][each]['rain'])):
+				print("It is Raining in ", obj['city']['name'], " on ", obj['list'][each]['dt_txt'])
+			'''
+
+
+
 
 def view_map(url2,report_type):
 	webbrowser.open(url2)
+	print("Opening Weather Map in New window")
 
 def main():
 	jobs = []
 	ids=cfg.city_id
 	for each in ids:
-		fiveday = threading.Thread(target=get_data, args=(cfg.url_5day+str(each)+'&APPID='+cfg.api_key,"5day/3hr"))
+		fiveday = threading.Thread(target=get_data, args=(cfg.url_5day+str(each)+'&APPID='+cfg.api_key,"fiveday"))
 		jobs.append(fiveday)
 		weather_map = threading.Thread(target=get_data, args=(cfg.url_weather+cfg.api_key+"&id="+str(each),"weather_map"))
 		jobs.append(weather_map)
