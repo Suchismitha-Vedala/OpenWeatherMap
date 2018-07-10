6a2cf65f1a1d9600d38d255e10c09073a592be76
Suchismitha Vedala <svedala@uh.edu>
2018-07-10 18:13:26 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_key = "b9428de8e646a42987eee11eced64b1b"
+api_key = "xxxxxxxxxxxxxxxxxxxxxx"
 url_5day= "http://api.openweathermap.org/data/2.5/forecast?id="
 url_16day ="http://api.openweathermap.org/data/2.5/forecast/daily?id="
 url_weather = "http://api.openweathermap.org/data/2.5/weather?APPID="
